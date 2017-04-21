79e0c702f5752751aa44b44e31d58fd0b9137dad
Matthew Moreno <mmore500@gmail.com>
2017-04-21 12:56:18 -0700

    Correct printed instructions

--- flsk.py
+++ flsk.py
@@ -11,5 +11,5 @@ app = flask.Flask(__name__, static_folder="force")
 @app.route('/<path:path>')
 def static_proxy(path):
   return app.send_static_file(path)
-print('\nGo to http://localhost:8000/force.html to see the example\n')
+print('\nGo to http://localhost:8000/force/index.html to see the example\n')
 app.run(port=8000)
